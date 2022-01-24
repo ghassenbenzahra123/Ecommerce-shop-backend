e0d723861a9afe2f1fba3bf17acdb7504dcf992b
ghassenbenzahra123 <ghassen.benzahra@esprit.tn>
2022-01-24 21:56:06 +0100

    Cart Crud

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
+
 const cors = require("cors");
 
 
@@ -25,6 +27,8 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/carts", cartRoute);
+
 
 
 app.listen(process.env.PORT || 5000, () => {
